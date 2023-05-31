e06a3c679edad6560576bee544ea3c110398256d
ibrahim-allaham <ibrahemlhm@yahoo.com>
2023-05-31 10:32:12 +0200

    minor-change

--- src/components/countries/CountryItem.tsx
+++ src/components/countries/CountryItem.tsx
@@ -1,5 +1,31 @@
-import React from "react";
+import {Country} from "./CountryList";
 
-export default function CountryItem() {
-  return <div>CountryItem</div>;
+type Prop = {
+  country : Country;
+}
+
+export default function CountryItem({country}:Prop) {
+  return (
+    <div key={country.name.common} className="group relative">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+        <img
+          src={country.flags.png}
+          alt={country.name.common}
+          className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+        />
+      </div>
+      <div className="mt-4 flex justify-between ">
+        <div className="flex flex-col items-start">
+          <h3 className="text-sm text-gray-700">
+            <a href={country.maps.googleMaps}>
+              <span aria-hidden="true" className="absolute inset-0" />
+              {country.name.common}
+            </a>
+          </h3>
+          <p className="mt-1 text-sm text-gray-500">{country.region}</p>
+        </div>
+        <p className="text-sm font-medium text-gray-900">{country.capital}</p>
+      </div>
+    </div>
+  );
 }
--- src/components/countries/CountryList.tsx
+++ src/components/countries/CountryList.tsx
@@ -1,19 +1,20 @@
 import { useState, useEffect } from "react";
+import CountryItem from "./CountryItem";
+
+export type Name = {
+  common: string;
+  official: string;
+};
+
+export type Country = {
+  name: Name;
+  region: string;
+  flags: { [keys: string]: string };
+  maps: { [keys: string]: string };
+  capital: string[];
+};
 
 export default function CountryList() {
-  type Name = {
-    common: string;
-    official: string;
-  };
-
-  type Country = {
-    name: Name;
-    region: string;
-    flags: { [keys: string]: string };
-    maps: { [keys: string]: string };
-    capital: string[];
-  };
-
   const [countries, setCountries] = useState<Country[]>([]);
 
   async function getProducts(url: string): Promise<void> {
@@ -38,31 +39,7 @@ export default function CountryList() {
         </h2>
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {countries.map((country) => (
-            <div key={country.name.common} className="group relative">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <img
-                  src={country.flags.png}
-                  alt={country.name.common}
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <a href={country.maps.googleMaps}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {country.name.common}
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{country.region}</p>
-                </div>
-                <p className="text-sm font-medium text-gray-900">
-                  {country.capital}
-                </p>
-              </div>
-            </div>
-          ))}
+          {countries.map((country) => <CountryItem country={country} />)}
         </div>
       </div>
     </div>
